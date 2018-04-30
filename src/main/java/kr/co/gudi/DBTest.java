603a1f4b9b9ab5a97da8b4b2d809a9a4f9995f33
GOODEE <GOODEE@DESKTOP-ARIPPJJ>
2018-04-30 15:04:46 +0900

    테스트

---
+++ src/main/java/kr/co/gudi/DBTest.java
@@ -0,0 +1,38 @@
+package kr.co.gudi;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class DBTest extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet()");
+		
+		String resource = "kr/co/gudi/mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		
+		System.out.println(sqlSessionFactory);
+		
+		SqlSession session = sqlSessionFactory.openSession(true);
+		HashMap<String, Object> result = session.selectOne("test.selectTest");
+		System.out.println(result);
+		System.out.println("End");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+}
